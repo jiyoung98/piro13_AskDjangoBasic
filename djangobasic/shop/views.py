fce1be9a19027b7bf2fd14bb74ef2b7d69549170
jiyoung98 <jiyoung15105@naver.com>
2020-07-24 19:01:21 +0900

    askdjango강의 9강 장고모델(ORM)까지

--- djangobasic/shop/views.py
+++ djangobasic/shop/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from urllib.parse import quote
 
 # Create your views here.
 
 def archives_year(request, year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
+
+def view1(request):
+    return HttpResponse('Hello, jiyoung')
